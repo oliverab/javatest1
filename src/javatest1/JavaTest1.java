de067bb5806d670e55ef780cc2176434fe0c66e9
Oliver Broad <oliverab@users.noreply.github.com>
2017-01-15 15:07:36 +0000

    First commit

---
+++ src/javatest1/JavaTest1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatest1;
+
+/**
+ *
+ * @author Oliver
+ */
+import javax.swing.JOptionPane; //import class JOptionPane
+public class JavaTest1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //System.out.println("Hello!!!");
+        JOptionPane.showMessageDialog(null,"Hello\nfrom\njava");
+        // TODO code application logic here
+        System.exit(0);
+    }
+    
+}
